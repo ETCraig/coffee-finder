8db7b458c2ed2786771f9f793a17287762b0334b
Craig <dev.etcraig@gmail.com>
2023-01-07 19:23:55 -0700

    Initial Banner & Card Built Out - Images & Fonts Added

---
+++ components/card/card.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import cls from "classnames";
+
+import styles from "./card.module.css";
+
+const Card = (props) => {
+  return (
+    <Link href={props.href} className={styles.cardLink}>
+      <div className={cls("glass", styles.container)}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{props.name}</h2>
+        </div>
+        <div className={styles.cardImageWrapper}>
+          <Image
+            src={props.imgUrl}
+            width={260}
+            height={260}
+            className={styles.cardImage}
+          />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Banner from "../components/banner";
+import Banner from "../components/banner/banner";
+import Image from "next/image";
+import Card from "../components/card/card";
+import coffeeStores from "../data/coffee-stores.json";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
@@ -13,11 +16,32 @@ export default function Home() {
         <title>Coffee Finder</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Banner
-        buttonText="View store nearby"
-        handleOnClick={handleOnBannerBtnClick}
-      />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
+        <div className={styles.heroImage}>
+          <Image
+            src="/static/hero-image.png"
+            height={400}
+            width={700}
+            alt="Hero Image"
+          />
+        </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((store) => {
+            return (
+              <Card
+                name={store.name}
+                imgUrl={store.imgUrl}
+                href={`/coffee-store/${store.id}`}
+                className={styles.card}
+              />
+            );
+          })}
+        </div>
+      </main>
     </>
   );
 }
